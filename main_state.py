1b0f9d9e4897a126dcd3fe378965a20807ce71ae
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2021-10-21 01:32:37 +0900

    골렘 솔져 완성

--- main_state.py
+++ main_state.py
@@ -158,7 +158,7 @@ class Slime:
         self.delay = 0
         self.expelCount = False
         self.expelnum = 0
-        self.i = 0.1
+        self.i = 0.2
         self.walk = load_image("monster/slime/slime_walk.png")
         self.attack = load_image("monster/slime/slime_hit_attack.png")
         self.absorbed = load_image("monster/slime/slimeabsorbed_attack.png")
@@ -233,17 +233,72 @@ class GolemSoldier:
         self.x = random.randint(200, get_canvas_width() - 200)
         self.y = random.randint(200, get_canvas_height() - 100)
         self.animationX = 0
-        self.animationY = 0
+        self.animationDir = 0
         self.statement = 0
         self.delay = 0
-        self.i = 0.1
-        self.walk = load_image("monster/GolemSoldier/GolemSoldier_Attack_4.png")
+        self.i = 0.2
+        self.walk = load_image("monster/GolemSoldier/GolemSoldier_Floating.png")
+        self.attack = load_image("monster/GolemSoldier/GolemSoldier_Attack.png")
 
     def draw(self):
-        self.walk.clip_draw(100 * self.animationX, 0, 100, 100, self.x, self.y)
+        if self.statement == 0:
+            self.walk.clip_draw(200 * self.animationX, 200 * self.animationDir, 200, 200, self.x, self.y)
+        else:
+            self.attack.clip_draw(200 * self.animationX, 200 * self.animationDir, 200, 200, self.x, self.y)
 
     def update_animation(self):
-        self.animationX = (self.animationX + 1) % 13
+        if self.statement == 0:
+            self.animationX = (self.animationX + 1) % 8
+        else:
+            self.animationX += 1
+            if self.animationX == 13:
+                self.statement = 0
+                self.delay = 20
+                self.animationX = 0
+    def move(self):
+        if self.delay == 0:
+            if self.statement == 0:
+                t = self.i / 100
+                self.x = (1 - t) * self.x + t * character.x
+                self.y = (1 - t) * self.y + t * character.y
+                if self.x < character.x:
+                    dx = character.x - self.x
+                    if self.y < character.y:
+                        dy = character.y - self.y
+                        if dx < dy:
+                            self.animationDir = 0
+                        else:
+                            self.animationDir = 3
+
+                    else:
+                        dy = self.y - character.y
+                        if dx < dy:
+                            self.animationDir = 1
+                        else:
+                            self.animationDir = 3
+                else:
+                    dx = self.x - character.x
+                    if self.y < character.y:
+                        dy = character.y - self.y
+                        if dx < dy:
+                            self.animationDir = 0
+                        else:
+                            self.animationDir = 2
+
+                    else:
+                        dy = self.y - character.y
+                        if dx < dy:
+                            self.animationDir = 1
+                        else:
+                            self.animationDir = 2
+
+                if (self.x - character.x) ** 2 + (self.y - character.y) ** 2 < 6000:
+                    self.statement = 1
+                    self.animationX = 0
+            else:
+                pass
+        else:
+            self.delay -= 1
 
 
 def check_slime():
@@ -336,6 +391,7 @@ def update():
     character.update_character()
     for i in range(SlimeNum):
         slime[i].move()
+    golemsoldier.move()
     AnimationClock = (AnimationClock + 1) % 100
 
 
