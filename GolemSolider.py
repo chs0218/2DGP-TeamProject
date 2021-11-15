91d0363130af38427dfe0b83f0167ceab78a4d6c
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2021-11-15 17:22:00 +0900

    golemSoldier 작업 시작

--- GolemSolider.py
+++ GolemSolider.py
@@ -1,3 +1,93 @@
+from pico2d import *
+import game_framework
+import random
+
+TURN_TO_MOVESTATE, TURN_TO_ATTACKSTATE, TURN_TO_DEADSTATE = range(3)
+
+
+class MoveState:
+    def enter(golemsoldier, event):
+        pass
+
+    def exit(golemsoldier, event):
+        pass
+
+    def do(golemsoldier):
+        from main_state import character
+        pass
+
+    def draw(golemsoldier):
+        golemsoldier.walk.clip_draw(200 * golemsoldier.animationX, 200 * golemsoldier.animationDir,
+                                    200, 200, golemsoldier.x, golemsoldier.y)
+        pass
+
+
+class AttackState:
+    def enter(golemsoldier, event):
+        pass
+
+    def exit(golemsoldier, event):
+        pass
+
+    def do(golemsoldier):
+        pass
+
+    def draw(golemsoldier):
+        pass
+
+class DeadState:
+    def enter(golemsoldier, event):
+        pass
+
+    def exit(golemsoldier, event):
+        pass
+
+    def do(golemsoldier):
+        pass
+
+    def draw(golemsoldier):
+        pass
+
+
+next_state_table = [MoveState, AttackState, DeadState]
+
+
+class golemsoldier:
+    die = None
+    walk = None
+    attack = None
+
+    def __init__(self):
+        self.x = random.randint(200, get_canvas_width() - 200)
+        self.y = random.randint(200, get_canvas_height() - 100)
+        self.animationX = 0
+        self.animationDir = 0
+        self.cur_state = 0
+        self.hp = 1
+        self.dead = False
+        self.delay = 0
+        self.i = 0.2
+        if golemsoldier.die == None:
+            golemsoldier.walk = load_image("monster/GolemSoldier/GolemSoldier_Floating.png")
+            golemsoldier.attack = load_image("monster/GolemSoldier/GolemSoldier_Attack.png")
+            golemsoldier.die = load_image("monster/GolemSoldier/soldier_die.png")
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[event]
+            self.cur_state.enter(self, event)
+
+
 # from pico2d import *
 # import random
 #
